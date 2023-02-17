7e5007cfadccbcb30e8f80f75638f4e0166f7a69
Denis Kelesidis <kelesidis@live.com>
2023-02-17 20:23:56 +0200

    Api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/bookings', 'App\Http\Controllers\Api\v1\BookingController@getBookings');
+Route::post('/bookings', 'App\Http\Controllers\Api\v1\BookingController@bookProduct');
