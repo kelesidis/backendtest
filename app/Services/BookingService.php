87e02ff89fb66d75063c15d2e39c9a2f1ee033a4
Denis Kelesidis <kelesidis@live.com>
2023-02-17 22:48:56 +0200

    Optimizing Booking Service +Eager Loading,exception,logs

--- app/Services/BookingService.php
+++ app/Services/BookingService.php
@@ -2,73 +2,79 @@
 
 namespace App\Services;
 
-
 use App\Models\Client;
 use App\Models\Booking;
 use App\Models\Product;
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class BookingService{
 
     public function getBookings($request)
     {
-    $bookings = Booking::all();
+        try {
+            $bookings = Booking::with('product')->get();
 
-    $data = [];
-    foreach ($bookings as $booking) {
-        $product = $booking->product;
+            $data = $bookings->map(function ($booking) {
+                return [
+                    'id' => $booking->id,
+                    'client_id' => $booking->client_id,
+                    'product_id' => $booking->product_id,
+                    'booked_on' => $booking->booked_on,
+                    'is_available' => $booking->product->bookings->count() < $booking->product->capacity ? 'Available' : 'Unavailable',
+                ];
+            });
+            Log::info('search', ['get booking' => $data]);
+            return response()->json(['data' => $data], 200);
+        } catch (\Exception $e) {
+            Log::error('Error while getting bookings: ' . $e->getMessage());
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
 
-        $isAvailable = $product->bookings->count() < $product->capacity;
-
-        $data[] = [
-            'id' => $booking->id,
-            'client_id' => $booking->client_id,
-            'product_id' => $booking->product_id,
-            'booked_on' => $booking->booked_on,
-            'is_available' => $isAvailable ? 'Available' : 'Unavailable',
-        ];
     }
 
-    return response()->json(['data' => $data], 200);
-
-    }
 
     public function bookProduct($request)
     {
-        // dd($request);
-        $clientId = $request->input('client_id');
-        $productId = $request->input('product_id');
-        $bookedOn = $request->input('booked_on');
+        try {
+            $validator = Validator::make($request->all(), [
+                'client_id' => 'required|exists:clients,id',
+                'product_id' => 'required|exists:products,id',
+                'booked_on' => 'required',
+            ]);
 
-        // Check if client exists
-        $client = Client::find($clientId);
-        if (!$client) {
-            return response()->json(['message' => 'Client not found'], 404);
+            if ($validator->fails()) {
+                return response()->json(['message' => $validator->errors()->first()], 400);
+            }
+
+            $clientId = $request->input('client_id');
+            $productId = $request->input('product_id');
+            $bookedOn = $request->input('booked_on');
+
+            $product = Product::findOrFail($productId);
+
+            if ($product->bookings->count() >= $product->capacity) {
+                return response()->json(['message' => 'Product is fully booked'], 400);
+            }
+
+            $client = Client::findOrFail($clientId);
+
+            if ($client->bookings()->where('product_id', $productId)->exists()) {
+                return response()->json(['message' => 'Client has already booked this product'], 400);
+            }
+
+            $booking = new Booking();
+            $booking->client_id = $clientId;
+            $booking->product_id = $productId;
+            $booking->booked_on = $bookedOn;
+            $booking->save();
+
+            return response()->json(['message' => 'Booking created successfully'], 201);
+        } catch (Exception $e) {
+            Log::error('Error while saving bookings: ' . $e->getMessage());
+            return response()->json(['message' => 'An error occurred while creating the booking'], 500);
         }
-
-        // Check if product exists
-        $product = Product::find($productId);
-        if (!$product) {
-            return response()->json(['message' => 'Product not found'], 404);
-        }
-
-        // Check if product is already fully booked
-        if ($product->bookings->count() >= $product->capacity) {
-            return response()->json(['message' => 'Product is fully booked'], 400);
-        }
-
-        // Check if client has already booked this product
-        if ($client->bookings()->where('product_id', $productId)->exists()) {
-            return response()->json(['message' => 'Client has already booked this product'], 400);
-        }
-
-        // Create a new booking
-        $booking = new Booking();
-        $booking->client_id = $clientId;
-        $booking->product_id = $productId;
-        $booking->booked_on = $bookedOn;
-        $booking->save();
-
-        return response()->json(['message' => 'Booking created successfully'], 201);
     }
 
-}
+    }
