642931b6e0857f72d6a9cf1173f6894618f5dced
Denis Kelesidis <kelesidis@live.com>
2023-02-17 20:03:08 +0200

    Booking Model, Migration

---
+++ app/Models/Booking.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
